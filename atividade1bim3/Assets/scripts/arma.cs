e9a78c951b35e54666a96534eb6776e06187e85f
wzinn <ws91443973@gmail.com>
2025-09-29 11:42:37 -0300

    29/09
    slider

--- atividade1bim3/Assets/scripts/arma.cs
+++ atividade1bim3/Assets/scripts/arma.cs
@@ -1,25 +1,54 @@
-
 using UnityEngine;
 
 public class Arma : MonoBehaviour
 {
-    public Transform saidadetiro;
-
+    public Transform saidaDoTiro;
     public GameObject bala;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float intevaloDeDisparo = 0.2f;
+    
+    private float tempoDeDisparo = 0;
+    
+    private Camera camera;
+    public GameObject cursor;
     void Start()
     {
-        
+        camera = Camera.main;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+         
+        // Distância da câmera ao objeto. Precisamos disso para fazer o cálculo correto.
+        float camDis = camera.transform.position.y - transform.position.y;
+
+        // Obtém a posição do mouse no espaço mundial. Usando camDis para o eixo Z.
+        Vector3 mouse = camera.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, camDis));
+
+        float AngleRad = Mathf.Atan2 (mouse.y - transform.position.y, mouse.x - transform.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+
+        transform.rotation =  Quaternion.AngleAxis( angle, Vector3.forward);
+       
+        Debug.Log("Angilo: "+angle);
+        
+        cursor.transform.position = new Vector3(mouse.x, mouse.y, cursor.transform.position.z);
+        
+        Debug.DrawLine(transform.position, mouse , Color.red);
+        
+        
+        if (tempoDeDisparo <=0 && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            GameObject  b =Instantiate(this.bala.gameObject,saidadetiro.position,saidadetiro.rotation) as GameObject;
+            
+            Debug.Log("Bala disparada");
+         
+            GameObject b = Instantiate (this.bala,saidaDoTiro.position, saidaDoTiro.rotation) as GameObject;
+            
+            tempoDeDisparo = intevaloDeDisparo;
         }
 
+        if (tempoDeDisparo > 0)
+        {
+            tempoDeDisparo -= Time.deltaTime;
+        }
 
     }
 }
--- atividade1bim3/Assets/scripts/bala.cs
+++ atividade1bim3/Assets/scripts/bala.cs
@@ -1,54 +1,53 @@
-using System;
 using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
-    [SerializeField] private int dano;
-    [SerializeField] private float velocidade;
+    [SerializeField] private int dano = 1;
+    [SerializeField] private float velocidade = 1.5f;
 
+    private Renderer m_Renderer;
+    
     public void setDano(int dano)
     {
         this.dano = dano;
     }
-
     public int getDano()
     {
         return this.dano;
     }
-
-    public void setVelocidade(float velocidade)
-    {
-        this.velocidade = velocidade;
-    }
-
-    public float getVelocidade()
-    {
-        return this.velocidade;
-    }
-
+    
+    
     void Start()
     {
-        
+        m_Renderer = GetComponent<Renderer>();
     }
+
     
     void Update()
     {
-        // transform.Translate(Vector3.right * velocidade * Time.deltaTime);
-        // transform.Translate( new Vector3(velocidade * Time.deltaTime,0,0));
-        transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+        transform.Translate(velocidade * Time.deltaTime, 0, 0);
+
+        if (!m_Renderer.isVisible)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.gameObject.CompareTag("Inimigo"))
+        if (collision.gameObject.CompareTag("Inimigo"))
         {
-            int novaVida = 
-                other.gameObject.GetComponent<Personagem>().getVida() 
-                - getDano();
-            other.gameObject.GetComponent<Personagem>().setVida(novaVida);
+            // Causa dano ao Inimigo
+            int novaVida = collision.gameObject.GetComponent<Personagem>().getVida() - getDano();
+            collision.gameObject.GetComponent<Personagem>().setVida(novaVida);
+            
+            //collision.gameObject.GetComponent<Personagem>().recebeDano(getDano());
         }
-
+        
+        // desliga a bala apos a colisão
         //gameObject.SetActive(false);
+        
+        //destrtoi a bala apos a colisão
         Destroy(gameObject);
     }
 }
